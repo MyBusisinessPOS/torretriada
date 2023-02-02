9a8c05d2fc07982a3808e1bf26e4466deebc57db
Cesar Chab <cesarchabuluac@gmail.com>
2023-02-01 18:48:04 -0600

    Improve quoter

--- js/funciones-cotizador.js
+++ js/funciones-cotizador.js
@@ -31,8 +31,62 @@ $(window).load(function () {
 	});
 });
 
+const populateData = (data, option = "") => {
+	if (data) {
+
+		// const filter = data.filter(lote => lote.lote.startsWith("D"));
+		// console.log(filter)
+
+		for (var i in data) {
+			var enlace = $("svg #" + data[i].lote);
+
+			const filterLote = data[i].lote.startsWith(option)
+			// console.log(filterLote)
+
+			if (data[i].status == 1) {				
+				if(option == "" || filterLote) {
+					$("svg #" + data[i].lote).addClass("label-success-cotizador");
+					$("svg #" + data[i].lote).css("cursor", "pointer");
+					$("svg #" + data[i].lote).css("fill", "rgba(117,209,152,0.50)");
+				} else {
+
+					$("svg #" + data[i].lote).removeClass("label-success-cotizador");
+					$("svg #" + data[i].lote).css("cursor", "default");
+					$("svg #" + data[i].lote).css("fill", "rgb(169,169,169, 0.50)");
+				}
+				
+				//$(enlace).attr("data-toggle","popover");
+				//$(enlace).attr("data-trigger","hover");
+				//$(enlace).attr("data-placement","top");
+				//$(enlace).attr("data-content",'<h4 class="content-title">'+data[i].lote+' </h4><p class="content-text"><span class="p-metros">'+data[i].m2Formato+' m<sup>2</sup></span><br /><span class="p-price">$'+data[i].precioFormato+'</span><br /><span class="p-disponibilidad">Disponible</span></p>');
+			}
+			if (data[i].status == 2) {
+				if(option == "") {
+					$("svg #" + data[i].lote).css("fill", "rgba(232,174,0,0.50)");
+				}
+				
+				//$(enlace).attr("data-toggle","popover");
+				//$(enlace).attr("data-trigger","hover");
+				//$(enlace).attr("data-placement","top");
+				//$(enlace).attr("data-content",'<h4 class="content-title">'+data[i].lote+' </h4><p class="content-text"><span class="p-metros">'+data[i].m2Formato+' m<sup>2</sup></span><br /><span class="p-disponibilidad">Apartado</span></p>');
+			}
+			if (data[i].status == 3) {
+				if(option == "") {
+					$("svg #" + data[i].lote).css("fill", "rgba(175,17,24,0.50)");
+				}
+				
+				//$(enlace).attr("data-toggle","popover");
+				//$(enlace).attr("data-trigger","hover");
+				//$(enlace).attr("data-placement","top");
+				//$(enlace).attr("data-content",'<h4 class="content-title">'+data[i].lote+' </h4><p class="content-text"><span class="p-metros">'+data[i].m2Formato+' m<sup>2</sup></span><br /><span class="p-disponibilidad">Vendido</span></p>');
+			}
+		};
+	}
+}
+
 
 $(function ($) {
+
 	//dispositivo();
 	$.ajax({
 		url: 'controller/controllerLotes.php',
@@ -43,45 +97,28 @@ $(function ($) {
 		success: function (data) {
 			lotes = JSON.parse(data)
 			// lotes = JSON.parse(data);
-			 console.log(data);
+			//  console.log(data);
 		}
 	}).done(function () {
-		for (var i in lotes) {
-			var enlace = $("svg #" + lotes[i].lote);
-			//var enlaceA = $("svg #Lote_"+lotes[i].lote).parent();
-
-			//console.log(css_heredado);
-			if (lotes[i].status == 1) {
-				$("svg #" + lotes[i].lote).addClass("label-success-cotizador");
-				$("svg #" + lotes[i].lote).css("cursor", "pointer");
-				$("svg #" + lotes[i].lote).css("fill", "rgba(117,209,152,0.50)");
-				//$(enlace).attr("data-toggle","popover");
-				//$(enlace).attr("data-trigger","hover");
-				//$(enlace).attr("data-placement","top");
-				//$(enlace).attr("data-content",'<h4 class="content-title">'+lotes[i].lote+' </h4><p class="content-text"><span class="p-metros">'+lotes[i].m2Formato+' m<sup>2</sup></span><br /><span class="p-price">$'+lotes[i].precioFormato+'</span><br /><span class="p-disponibilidad">Disponible</span></p>');
-			}
-			if (lotes[i].status == 2) {
-				$("svg #" + lotes[i].lote).css("fill", "rgba(232,174,0,0.50)");
-				//$(enlace).attr("data-toggle","popover");
-				//$(enlace).attr("data-trigger","hover");
-				//$(enlace).attr("data-placement","top");
-				//$(enlace).attr("data-content",'<h4 class="content-title">'+lotes[i].lote+' </h4><p class="content-text"><span class="p-metros">'+lotes[i].m2Formato+' m<sup>2</sup></span><br /><span class="p-disponibilidad">Apartado</span></p>');
-			}
-			if (lotes[i].status == 3) {
-				$("svg #" + lotes[i].lote).css("fill", "rgba(175,17,24,0.50)");
-				//$(enlace).attr("data-toggle","popover");
-				//$(enlace).attr("data-trigger","hover");
-				//$(enlace).attr("data-placement","top");
-				//$(enlace).attr("data-content",'<h4 class="content-title">'+lotes[i].lote+' </h4><p class="content-text"><span class="p-metros">'+lotes[i].m2Formato+' m<sup>2</sup></span><br /><span class="p-disponibilidad">Vendido</span></p>');
-			}
-		};
+		populateData(lotes)
 	});
+
+
+	$('#select-type-lote').on('change', function (e) {
+		const option = $(this).val()		
+		populateData(lotes, option)		
+	})
 
 	$('body').on('touchstart click', 'polygon,path,rect', function (e) {
 		e.preventDefault();
-		
 		let _batch = $(this);
 		let _bid = _batch.attr('id');
+		const typeLote = $('#select-type-lote option:selected').val()
+		let active = true;
+		if (typeLote != '' || typeLote != null || typeLote != undefined) {
+			active = _bid.startsWith(typeLote)
+		}
+		
 		if (_bid) {
 			loteID = _bid;		
 
@@ -125,8 +162,10 @@ $(function ($) {
 								$('#modalCotizador .contiene-img').html("<img src='" + PATH + "img/imgLotes/" + _json.ruta + "' /><span class='borde'></span>");
 							}
 							$("#formContact")[0].reset();
-							$('#modalCotizador').modal("show");
-							console.log("Eduardo"._json.modelo);
+							if(active) {
+								$('#modalCotizador').modal("show");
+							}						
+							
 						}
 					}
 				}
